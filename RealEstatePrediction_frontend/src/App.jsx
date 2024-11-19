bb5b1234e282edcad9dc18564d862c6cfeaa106b
huda-elhirm <houdaelhirm@gmail.com>
2024-11-19 21:39:45 +0100

    fix user mongodb error

---
+++ RealEstatePrediction_frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import "./layout.scss"
+import Navbar from "./components/navbar/Navbar.jsx";
+
+function App() {
+
+    return (
+        <div className="layout">
+            <Navbar />
+        </div>
+    );
+}
+
+export default App
